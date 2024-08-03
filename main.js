8307f4c0f75ffff374cb522119542f27b16807e3
null-dimension <4hrishi4@gmail.com>
2024-08-03 22:18:35 +0530

    update rng script

--- main.js
+++ main.js
@@ -55,12 +55,20 @@ const fontFamilies = [
   "Courier New",
   "Brush Script MT",
 ];
-const rngfyButton = document.getElementById("rngfy");
+const uniqueTagsMap = {};
 
-function rngfy() {
-  const tags = document.querySelectorAll("*");
-  for (tag of tags) {
-    tag.style.fontSize = `${rng(12, 32)}px`;
+
+let tagsEligibleForRng = [];
+
+const rngfy = () => {
+
+  if(tagsEligibleForRng.length === 0) {
+    alert("Please select at least one tag to randomize from the menu!");
+    return;
+  }
+
+  for (tag of tagsEligibleForRng) {
+    tag.style.fontSize = `${rng(12, 24)}px`;
     tag.style.fontFamily = `${fontFamilies[rng(0, fontFamilies.length - 1)]}`;
     tag.style.background = `rgba(${rng(0, 255)}, ${rng(0, 255)}, ${rng(
       0,
@@ -72,17 +80,17 @@ function rngfy() {
     )}, ${rngFloat(0, 1)})`;
     //tag.style.transform = `rotate(${rng(-10, 10)}deg)`;
     tag.style.fontWeight = `${rng(10, 1000)}`;
-    tag.style.padding = `${rng(0, 64)}px`;
-    tag.style.margin = `${rng(0, 32)}px`;
-    tag.style.letterSpacing = `${rng(0, 10)}px`;
-    tag.style.boxShadow = `${rng(0, 10)}px ${rng(0, 10)}px ${rng(
+    tag.style.padding = `${rng(0, 16)}px`;
+    tag.style.margin = `${rng(0, 16)}px`;
+    tag.style.letterSpacing = `${rng(0, 5)}px`;
+    tag.style.boxShadow = `${rng(0, 5)}px ${rng(0, 5)}px ${rng(
       0,
-      10
-    )}px ${rng(0, 10)}px`;
-    tag.style.border = `${rng(0, 10)}px ${
+      5
+    )}px ${rng(0, 5)}px`;
+    tag.style.border = `${rng(0, 5)}px ${
       borderStyles[rng(0, borderStyles.length - 1)]
     } rgb(${rng(0, 255)}, ${rng(0, 255)}, ${rng(0, 255)}`;
-    tag.style.borderRadius = `${rng(0, 32)}px`;
+    tag.style.borderRadius = `${rng(0, 16)}px`;
     //tag.style.position = positions[rng(0, positions.length - 1)];
     tag.style.opacity = rngFloat(0.7, 1);
     // tag.style.top = `${rng(0, 100)}px`;
@@ -96,16 +104,59 @@ function rngfy() {
   }
 }
 
-function rng(min, max) {
+const rng = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function rngFloat(min, max) {
+const rngFloat = (min, max) => {
   return (Math.random() * (max - min + 1) + min).toFixed(2);
 }
 
-rngfyButton.addEventListener("click", (e) => {
-  rngfy();
-});
+const populateTagsInOptions = (tags) => {
+  const tagSelector = document.querySelector("#tag-selector");
+  for(const tag of tags) {
+    const option = document.createElement("option");
+    option.textContent = tag.localName;
+    option.value = tag.localName;
+    
+    if(!uniqueTagsMap[tag.localName]){
+      uniqueTagsMap[tag.localName] =  true;
+      tagSelector.appendChild(option);
+    }
+  }
 
-window.onload = rngfy();
+}
+
+const onSelectClick = (evt) => {
+  const selectedOptions = Array.from(evt.target.parentElement.options).filter(option => option.selected).map(tag => tag.value);
+  tagsEligibleForRng = document.querySelectorAll(selectedOptions.join(","));
+}
+
+
+window.onload = () => {
+  const tags = document.querySelectorAll("*");
+
+  const tagSelector = document.querySelector("#tag-selector");
+  const rngfyButton = document.getElementById("rngfy");
+
+  populateTagsInOptions(tags);
+  tagSelector.addEventListener("click", (evt) => onSelectClick(evt));
+  rngfyButton.addEventListener("click", (e) => {
+    rngfy();
+  });
+
+  const menuCloseButton = document.getElementById("btn-close");
+  menuCloseButton.addEventListener("click", (e) => {
+    document.getElementById("menu-modal").classList.toggle("display-none");
+  });
+
+  const menuButton = document.getElementById("btn-menu");
+  menuButton.addEventListener("click", (e) => {
+    document.getElementById("menu-modal").classList.toggle("display-none");
+  });
+}
+window.addEventListener("keydown", (event) => {
+  if(event.key.toLowerCase() === "r") {
+    rngfy();
+  }
+})
